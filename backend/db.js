9577fa5893cbbabe728ba3a9ccc308136ca41941
YAMUNA K <yamunak972006@gmail.com>
2026-04-15 18:46:12 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/farmer-buyer-marketplace', {
@@ -13,7 +12,6 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-
 module.exports = connectDB;
 mongoose.connection.once('open', () => {
   console.log("✅ Connected DB Name:", mongoose.connection.name);
